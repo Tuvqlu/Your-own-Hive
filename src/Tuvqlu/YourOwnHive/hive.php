ad75934837f9b226f5ff23949e73a043284e73b8
testdev <83214438+DanteDEVS@users.noreply.github.com>
2022-04-24 20:31:37 +0200

    just a event test

--- src/Tuvqlu/YourOwnHive/hive.php
+++ src/Tuvqlu/YourOwnHive/hive.php
@@ -5,13 +5,30 @@ namespace Tuvqlu\YourOwnHive;
 use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
 use pocketmine\utils\TextFormat;
+use pocketmine\event\Listener;
+use pocketmine\utils\Config;
+use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\event\entity\EntityTeleportEvent;
 
-class hive extends PluginBase{
+class hive extends PluginBase implements Listener{
 
     protected function onEnable() : void{  
         $this->getLogger()->info("Plugin has been Enabled");
+        $this->getServer()->getPluginManager()->registerEvents($this, $this);
         @mkdir($this->getDataFolder());
         $this->saveDefaultConfig();
     }
+    
+	public function onJoin(PlayerJoinEvent $event) : void{
+		$player = $event->getPlayer();
+        $this->sendTitle("Welcome!");
+	}
+    
+    public function onLevelChange(EntityTeleportEvent $event) : void{
+		$entity = $event->getEntity();
+		if($entity instanceof Player){
+            $this->sendTitle("Teleporting to World...");
+        }
+	}
 
 }
