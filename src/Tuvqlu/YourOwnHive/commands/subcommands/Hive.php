7c02ce85386d0290d3a4058c98d552555551c66b
Tuvqlu <98550458+Tuvqlu@users.noreply.github.com>
2022-05-06 11:43:16 -0400

    Update Hive.php

--- src/Tuvqlu/YourOwnHive/commands/subcommands/Hive.php
+++ src/Tuvqlu/YourOwnHive/commands/subcommands/Hive.php
@@ -170,3 +170,921 @@ class Hive extends BaseSubCommand {
 	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
 	}
 }
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
+declare(strict_types=1);
+
+namespace Tuvqlu\YourOwnHive\commands\subcommands;
+
+use pocketmine\command\CommandSender;
+use CortexPE\Commando\BaseSubCommand;
+
+/**
+ * Class Hive
+ * @package Tuvqlu\YourOwnHive\commands\subcommands
+ */
+class Hive extends BaseSubCommand {
+
+	/**
+	 * prepare
+	 *
+	 * @return void
+	 */
+	protected function prepare(): void {
+		// $this->setPermission("profanityfilter.add");
+	}
+
+	/**
+	 * onRun
+	 *
+	 * @param CommandSender $sender
+	 * @param string $aliasUsed
+	 * @param array $args
+	 *
+	 * @return void
+	 */
+	public function onRun(CommandSender $sender, string $aliasUsed, array $args): void {
+	}
+}
